85ae3d1bc64872d78e8fb63cffc08679f1b07ee9
Kalips0o <Kalipsoo92@gmail.com>
2025-01-31 15:53:51 +0300

    Svg loader. File loader. Button UI kit

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,20 +1,11 @@
-// Импортируем Webpack для использования его типов и интерфейсов
 import webpack from "webpack";
-
-// Импортируем плагин MiniCssExtractPlugin для извлечения CSS в отдельные файлы
 import MiniCssExtractPlugin from "mini-css-extract-plugin";
-
-// Импортируем тип BuildOptions, который содержит параметры конфигурации сборки
 import { BuildOptions } from "./types/config";
-
-// Экспортируем функцию buildLoaders, которая возвращает массив настроек загрузчиков для Webpack
 export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
 
     // Конфигурация для загрузки CSS и SCSS файлов
     const cssLoader = {
-        // Указываем регулярное выражение для файлов .scss и .sass
         test: /\.s[ac]ss$/i,
-        // Настраиваем последовательность загрузчиков
         use: [
             // В режиме разработки используем 'style-loader' для внедрения стилей в DOM
             // В режиме продакшн используем MiniCssExtractPlugin.loader для извлечения CSS в отдельный файл
@@ -33,24 +24,37 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
                     },
                 }
             },
-            // Используем sass-loader для преобразования SCSS/SASS в CSS
             'sass-loader',
         ]
     };
 
-    // Конфигурация для загрузки TypeScript файлов
+
     const typescriptLoader = {
-        // Указываем регулярное выражение для файлов .ts и .tsx
         test: /\.tsx?$/,
-        // Используем ts-loader для компиляции TypeScript в JavaScript
         use: 'ts-loader',
-        // Исключаем папку node_modules, чтобы ускорить процесс сборки
         exclude: /node_modules/,
     };
 
+
+    const svgLoader = {
+        test: /\.svg$/,
+        use: ['@svgr/webpack']
+    };
+
+    const fileLoader = {
+        test: /\.(png|jpe?g|gif|woff2|woff)$/i,
+        use: [
+            {
+                loader: 'file-loader'
+            }
+        ]
+    }
+
     // Возвращаем массив загрузчиков
     return [
-        typescriptLoader, // Загрузчик для TypeScript
-        cssLoader,        // Загрузчик для CSS/SCSS
+        typescriptLoader,
+        cssLoader,
+        svgLoader,
+        fileLoader
     ];
 }
